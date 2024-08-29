d0c93f15c1276cfa4d1af6b008a6fc2c8821d281
Petru Potrimba <ppotrimba@gmail.com>
2024-08-29 22:11:34 +0200

    add again 2. CalcolaPrezzoFinale

---
+++ 2. CalcolaPrezzoFinale/2. CalcolaPrezzoFinale/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GestioneScontiNegozio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Chiamate di test con verifica
+            VerificaCalcoloPrezzoFinale(50.0, 12, "", 540.0); // Test 1
+            VerificaCalcoloPrezzoFinale(30.0, 7, "PROMO20", 159.6); // Test 2
+            VerificaCalcoloPrezzoFinale(15.0, 3, "", 45.0); // Test 3
+        }
+
+        public static double CalcolaPrezzoFinale(double prezzoIniziale, int quantita, string codiceSconto = "")
+        {
+           
+        }
+
+        public static void VerificaCalcoloPrezzoFinale(double prezzoIniziale, int quantita, string codiceSconto, double risultatoAtteso)
+        {
+            double risultatoCalcolato = CalcolaPrezzoFinale(prezzoIniziale, quantita, codiceSconto);
+
+            if (risultatoCalcolato == risultatoAtteso)
+            {
+                Console.WriteLine($"Test riuscito: Prezzo finale calcolato {risultatoCalcolato} è uguale al risultato atteso {risultatoAtteso}.");
+            }
+            else
+            {
+                Console.WriteLine($"Test fallito: Prezzo finale calcolato {risultatoCalcolato} non è uguale al risultato atteso {risultatoAtteso}.");
+            }
+        }
+    }
+}
